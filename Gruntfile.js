26685e0849baccced01936e87191728b9348b18a
Sai Nagamalla <sai.nagamalla@gmail.com>
2017-05-05 15:53:20 +1000

    grunt-ftpush removed

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,6 +25,22 @@ module.exports = function(grunt){
         }
       }
     },
+    /********ftpush not working*****************/
+    /*ftpush:{
+      build:{
+        auth:{
+          host:'libappdev.latrobe.edu.au',
+          port:6022,
+          authKey:'sn'
+        },
+        src:'',
+        dest:'/data/web/public_html/ebureau2',
+        exclusions: ['/.git','/node_modules','/Thumbs.db','/books/Thumbs.db','*.DS_Store','/.ftppass','*.mp4','*.EXE'],
+        keep:['/node_modules/uikit/dist'],
+        simple:false,
+        useList:false
+      }
+    },*/
     watch:{
       styles:{
         files:['less/*.less'], // which files to watch
@@ -39,8 +55,9 @@ module.exports = function(grunt){
   //loading grunt plugins
   //grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-less');
+  //grunt.loadNpmTasks('grunt-ftpush');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   //Default task(s).
-  grunt.registerTask('default', ['less','watch']);
+  grunt.registerTask('default', ['less']);
 };
