7131c4773f0007e0eb4a488a715194a72fb8a7de
Sai Nagamalla <sai.nagamalla@gmail.com>
2017-05-11 13:00:42 +1000

    sending email
    Added function for sending emails

--- js/angular-functions.js
+++ js/angular-functions.js
@@ -1,6 +1,6 @@
 angular.module('la-trobe-ebureau',[])
-  .controller('contact-library',['$scope', function($scope){
-    $scope.info = [];
+  .controller('contact-library',['$scope','$http',function($scope, $http){
+    $scope.formData = [];
     $scope.setFields = function(){
       $scope.name = '';
       $scope.email = '';
@@ -11,10 +11,22 @@ angular.module('la-trobe-ebureau',[])
     $scope.failure = false;
     $scope.submit = function(){
         $scope.dname = $scope.name;
-        $scope.info = [$scope.name, $scope.email, $scope.message];
-        $scope.success = true;
-        $scope.setFields();
-        $scope.form.$setPristine();
-        $scope.form.$setUntouched();
+        $scope.formData = {name:$scope.name, email:$scope.email, message:$scope.message};
+        $http({
+          method: 'POST',
+          url: 'php/email.php',
+          data: $.param($scope.formData),
+          dataType:'json',
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded'}
+        }).then(function successCallback(response){
+          var data = response.data;
+          if(data.result)  $scope.success = true;
+          else $scope.failure = true;
+          $scope.setFields();
+          $scope.form.$setPristine();
+          $scope.form.$setUntouched();
+        }, function errorCallback(response) {
+          $scope.failure = true;
+        });
     };
   }]);
--- php/email.php
+++ php/email.php
@@ -1,32 +1,20 @@
 <?php
-
+$emailObj = new Email;
+$emailObj->sendEmail($_POST);
 final class Email{
-
-  private $email;
-
-  private function __construct($email){
-
-    $this->ensureIsValidEmail($email);
-    $this->email =$email;
-  }
-
-  public static function fromString($email){
-    return new self($email);
-  }
-
-  public function __toString(){
-    return $this->email;
-  }
-
-  private function ensureIsValidEmail($email){
-    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-      throw new InvalidArgumentException(
-        sprintf(
-          '"%s" is not a valid email address', $email
-        )
-      );
-    }
+  public static function sendEmail($data){
+    $to='library@latrobe.edu.au';
+    $cc = $data['email'];
+    $from = 'noreply@latrobeebureau.latrobe.edu.au';
+    $subject = "La Trobe eBureau - Tell us what you think";
+    $message = "<div style='font:12px Helvetica'>From: <b>". $data['name']."</b><br><br>Message:<br><br>". $data['message']."<div>";
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= 'From: '.$from."\r\n";
+    $headers .= 'Cc: '.$cc . "\r\n";
+    $ok = mail($to,$subject,$message,$headers);
+    if($ok) echo json_encode(array("result"=>true));
+    else echo json_encode(array("result"=>false));
   }
 }
-
 ?>
